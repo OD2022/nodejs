80b88fbc61f77b828778649fe69c912e7d9e9937
Emmanuella <105435095+OD2022@users.noreply.github.com>
2023-12-14 13:06:57 +0000

    Hmm like magic, just a comma made the code bad

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.post("/sign-in", (req, res) => {
 	const {email, user_password, user_role} = req.body;
 	const query = `SELECT *
                    FROM WovenUsers
-                   WHERE email = $1,
+                   WHERE email = $1
                      AND user_password = $2 AND user_role = $3`;
 	connection.query(query, [email, user_password, user_role], (err, results) => {
 		if (err) {
