0299edf80c32cc4f2b0e9d01f563619a9ca7c206
Emmanuella <105435095+OD2022@users.noreply.github.com>
2023-12-14 14:13:26 +0000

    Added registration

--- index.js
+++ index.js
@@ -61,6 +61,118 @@ app.post('/sign-in', async (req, res) => {
 
 
 
+app.post('/registerAdmin', (req, res) => {
+    const connection = req.db;
+    const {
+        email,
+        first_name,
+        last_name,
+        country,
+        user_password,
+        admin_tel_no,
+        address,
+        DOB,
+        sex
+    } = req.body;
+    const userInsertQuery = 'INSERT INTO WovenUsers(email, first_name, last_name, country, user_password, user_role) VALUES (?,?,?,?,?,?)';
+    const userInsertParams = [email, first_name, last_name, country, user_password, 'admin'];
+    connection.query(userInsertQuery, userInsertParams, (err, userResults) => {
+        if (err) {
+            return connection.rollback(() => {
+                res.status(500).json({error: 'Error inserting into user table'});
+            });
+        }
+        const adminInsertQuery = 'INSERT INTO WovenAdmin(email, admin_tel_no, address, dob, sex) VALUES (?,?,?,?,?)';
+        const adminInsertParams = [email, admin_tel_no, address, DOB, sex];
+        connection.query(adminInsertQuery, adminInsertParams, (err) => {
+            if (err) {
+                return connection.rollback(() => {
+                    res.status(500).json({error: 'Error inserting into admin table'});
+                });
+            } else {
+                res.status(201).send('admin registered successfully');
+            }
+        });
+    });    
+});
+
+//Registering a seller
+app.post('/registerSeller', (req, res) => {
+    const connection = req.db;
+    const {
+        email,
+        first_name,
+        last_name,
+        country,
+        user_password,
+        ghana_region,
+        seller_tel_no,
+        momo_number,
+        address,
+        dob,
+        sex
+    } = req.body;
+    const userInsertQuery = 'INSERT INTO WovenUsers(email, first_name, last_name, country, user_password, user_role) VALUES (?,?,?,?,?,?)';
+    const userInsertParams = [email, first_name, last_name, country, user_password, 'seller'];
+    connection.query(userInsertQuery, userInsertParams, (err, userResults) => {
+        if (err) {
+            return connection.rollback(() => {
+                res.status(500).json({error: 'Error inserting into user table'});
+            });
+        }
+        const sellerInsertQuery = 'INSERT INTO Seller(email, ghana_region, seller_tel_no, momo_number, address, dob, sex) VALUES (?,?,?,?,?,?,?)';
+        const sellerInsertParams = [email, ghana_region,
+            seller_tel_no,
+            momo_number,
+            address,
+            dob,
+            sex];
+
+        connection.query(sellerInsertQuery, sellerInsertParams, (err) => {
+            if (err) {
+                return connection.rollback(() => {
+                    res.status(500).json({error: 'Error inserting into seller table'});
+                });
+            } else {
+                res.status(201).send('seller registered successfully');
+            }
+        });
+    });    
+});
+
+app.post('/registerCustomer', (req, res) => {
+    const connection = req.db;
+    const {
+        email,
+        first_name,
+        last_name,
+        country,
+        user_password
+    } = req.body;
+    const userInsertQuery = 'INSERT INTO WovenUsers(email, first_name, last_name, country, user_password, user_role) VALUES (?,?,?,?,?,?)';
+    const userInsertParams = [email, first_name, last_name, country, user_password, 'customer'];
+    connection.query(userInsertQuery, userInsertParams, (err, userResults) => {
+        if (err) {
+            return connection.rollback(() => {
+                res.status(500).json({error: 'Error inserting into user table'});
+            });
+        }
+        const customerInsertQuery = 'INSERT INTO Customer(email) VALUES (?)';
+        const customerInsertParams = [email, ghana_region];
+
+        connection.query(customerInsertQuery, customerInsertParams, (err) => {
+            if (err) {
+                return connection.rollback(() => {
+                    res.status(500).json({error: 'Error inserting into customer table'});
+                });
+            } else {
+                res.status(201).send('customer registered successfully');
+            }
+        });
+    });    
+});
+
+
 //Creating a cart
 app.post('/create-cart', async (req, res) => {
     try {
