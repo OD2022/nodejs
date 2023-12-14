d15d2d00c6d3e72225133328b36402363c1b2ba2
Emmanuella <105435095+OD2022@users.noreply.github.com>
2023-12-14 19:19:47 +0000

    Great Changes
    Great Changes

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const port = process.env.PORT || 3000;
 
 const { Pool } = require('pg');
 const cors = require('cors');
+
 app.use(express.json());
 app.use(cors());
 
@@ -181,6 +182,7 @@ app.post('/clear-cart', async (req, res) => {
             res.status(201).send('Cart succesfully emptied');
           })
         });
+
         
 //Checkout the cart
 app.post('/checkout/:cart-id', async (req, res) => {
@@ -215,391 +217,73 @@ app.post('/checkout/:cart-id', async (req, res) => {
         });
 
 
-
-app.get('/customer/merchant-order/:offset', async (req, res) => {
-    try {
-        const userProvidedOffset = parseInt(req.params.offset, 10) || 0;
-        const customer_email = req.body;
-        const sql = 'SELECT Orders.*, MerchantProductOrder.* FROM Orders JOIN MerchantProductOrder ON Orders.payment_id = MerchantProductOrder.payment_id WHERE Orders.customer_email = $1 ORDER BY Orders.date_created LIMIT 10 OFFSET $2';
-        const result = await connection.query(sql, [customer_email, userProvidedOffset]);
-        res.json(result.rows);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({error: 'Internal Server Error'});
-    }
-});
-
-//Getting stole product orders
-app.get('/customer/stole-order/:offset', async (req, res) => {
-    try {
-        const userProvidedOffset = parseInt(req.params.offset, 10) || 0;
-        const customer_email = req.body;
-        const sql = 'SELECT Orders.*, StoleProductOrder.* FROM Orders JOIN StoleProductOrder ON Orders.payment_id = StoleProductOrder.payment_id WHERE Orders.customer_email = $1 ORDER BY Orders.date_created LIMIT 10 OFFSET $2';
-        const result = await connection.query(sql, [customer_email, userProvidedOffset]);
-        res.json(result.rows);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({error: 'Internal Server Error'});
-    }
-});
-
-
-//Getting custom product orders
-app.get('/customer/customfabric-order/:offset', async (req, res) => {
-    try {
-        const userProvidedOffset = parseInt(req.params.offset, 10) || 0;
-        const customer_email = req.body;
-        const sql = 'SELECT Orders.*, CustomFabricOrderDetails.* FROM Orders JOIN CustomFabricOrderDetails ON Orders.payment_id = CustomFabricOrderDetails.payment_id WHERE Orders.customer_email = $1 ORDER BY Orders.date_created LIMIT 10 OFFSET $2';
-        const result = await connection.query(sql, [customer_email, userProvidedOffset]);
-        res.json(result.rows);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({error: 'Internal Server Error'});
-    }
-});
-
-
-//Getting all custom fabric order details by email
-app.get('/customfabric-order', async (req, res) => {
-    try {
-        const customer_email = req.body;
-        const sql = 'SELECT Orders.*, CustomFabricOrderDetails.* FROM Orders JOIN CustomFabricOrderDetails ON Orders.payment_id = CustomFabricOrderDetails.payment_id  WHERE Orders.customer_email = $1';
-        const result = await connection.query(sql, [customer_email]);
-        res.json(result.rows);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({error: 'Internal Server Error'});
-    }
-});
-
-
-//Placing an order for a product
-app.post('/customer/order', async (req, res) => {
-    try {
-        const {
-            payment_id,
-            customer_email,
-            country,
-            postal_code,
-            delivery_address,
-            delivery_method,
-            total_price
-        } = req.body;
-        const sql = 'INSERT INTO Orders(payment_id, customer_email, country, postal_code, delivery_address, delivery_method, total_price) VALUES(?,?,?,?,?,?,?)';
-        const result = await connection.query(sql, [ payment_id,
-            customer_email,
-            country,
-            postal_code,
-            delivery_address,
-            delivery_method,
-            total_price]);
-        res.json(result.rows);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({error: 'Internal Server Error'});
-    }
-});
-
-
-//Setting an order for a particular merchant's product
-app.post('customer/merchant-order', async (req, res) => {
-    try {
-        const {
-            payment_id,
-            product_id,
-            product_total,
-            quantity,
-            seller_fulfilled,
-            delivery_status,
-            tracking_id,
-            order_status
-        } = req.body;
-        const sql = 'INSERT INTO MerchantProductOrder(payment_id,product_id, product_total,quantity, seller_fulfilled, delivery_status, tracking_id, order_status) VALUES($1,$2,$3,$4,$5,$6,$7,$8)';
-        const result = await connection.query(sql, [payment_id,product_id, product_total,quantity, seller_fulfilled, delivery_status, tracking_id, order_status]);
-        res.json(result.rows);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({error: 'Internal Server Error'});
-    }
-});
-
-
-//Setting order for a particular stole
-app.post('customer/stole-order', async (req, res) => {
-    try {
-        const {
-            payment_id,
-            quantity,
-            color,
-            logo_link,
-            final_design_link,
-            delivery_status,
-            tracking_id,
-            order_status
-        } = req.body;
-        const sql = 'INSERT INTO StoleProductOrder(payment_id, quantity, color, logo_link, final_design_link, delivery_status, tracking_id, order_status) VALUES(?,?,?,?,?,?,?,?)';
-        const result = await connection.query(sql, [payment_id,
-            quantity,
-            color,
-            logo_link,
-            final_design_link,
-            delivery_status,
-            tracking_id,
-            order_status]);
-        res.json(result.rows);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({error: 'Internal Server Error'});
-    }
-});
-
-
-//Setting order details for custom-fabric
-app.post('customer/customfabric-order', async (req, res) => {
-    try {
-        const {
-            payment_id,
-            product_total,
-            final_design_link,
-            yards,
-            delivery_status,
-            tracking_id,
-            order_status
-        } = req.body;
-        const sql = 'INSERT INTO CustomFabricOrderDetails(payment_id,product_total,final_design_link, yards, delivery_status, tracking_id, order_status) VALUES(?,?,?,?,?,?,?)';
-        const result = await connection.query(sql, [payment_id,product_total,final_design_link, yards, delivery_status,
-            tracking_id,
-            order_status]);
-        res.json(result.rows);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({error: 'Internal Server Error'});
-    }
-});
-
-
-//User Views All MarketPlace Products
-app.get('/products/rows/:offset', async (req, res) => {
-    try {
-        const userProvidedOffset = parseInt(req.params.offset, 10) || 0;
-        const sql = 'SELECT * FROM Product ORDER BY date LIMIT 10 OFFSET $1';
-        const result = await connection.query(sql, [userProvidedOffset]);
-        res.json(result.rows);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({error: 'Internal Server Error'});
-    }
-});
-
-
-//User Views All Stole Designs
-app.get('/stoles/rows/:offset', async (req, res) => {
-    try {
-        const userProvidedOffset = parseInt(req.params.offset, 10) || 0;
-        const sql = 'SELECT * FROM StoleProduct ORDER BY date LIMIT 10 OFFSET $1';
-        const result = await connection.query(sql, [userProvidedOffset]);
-        res.json(result.rows);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({error: 'Internal Server Error'});
-    }
-});
-
-
-//User Gets Fabric Templates
-app.get('/customfabrics/rows/:offset', async (req, res) => {
-    try {
-        const userProvidedOffset = parseInt(req.params.offset, 10) || 0;
-        const sql = 'SELECT * FROM CustomFabricProduct ORDER BY date LIMIT 10 OFFSET $1';
-        const result = await connection.query(sql, [userProvidedOffset]);
-
-        res.json(result.rows);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({error: 'Internal Server Error'});
-    }
-});
+//Fetch all custom fabrics
+app.get('/customfabrics', async (req, res) => {
+    connection.query('SELECT * FROM CustomFabricProduct', (error, results) => {
+        if (error) {
+          throw error
+        }
+        res.status(201).json(results);
+      })
+    });
 
 
 //User views a single product
 app.get('/product/:id', (req, res) => {
-    const productId = req.params.id;
-    const sql = 'SELECT * FROM Product WHERE product_id = $1';
-    connection.query(sql, [productId], (err, result) => {
-        if (err) {
-            console.error('Error executing SQL query:', err);
-            res.status(500).send('Internal Server Error');
-        } else {
-            if (result.length > 0) {
-                res.json(result[0]); // Assuming the query returns one student
-            } else {
-                res.status(404).send('Product not found');
-            }
+    const productId = req.params['id'];
+    connection.query('SELECT * FROM Product WHERE product_id = $1', [productId], (error, results) => {
+        if (error) {
+          throw error
         }
+        res.status(201).json(results);
+      });
     });
-});
-
-//Fetch the stoles
-app.get('/stoles/:id', (req, res) => {
-    const stoleId = req.params.id;
-    // SQL query to fetch a student by ID
-    const sql = 'SELECT * FROM StoleProduct WHERE stole_product_id = $1';
-
-    connection.query(sql, [stoleId], (err, result) => {
-        if (err) {
-            console.error('Error executing SQL query:', err);
-            res.status(500).send('Internal Server Error');
-        } else {
-            if (result.length > 0) {
-                res.json(result[0]);
-            } else {
-                res.status(404).send('Product not found');
-            }
-        }
-    });
-});
-
-
-//Fetch the fabric
-app.get('/customfabric/:id', (req, res) => {
-    const customFabricId = req.params.id;
-    const sql = 'SELECT * FROM CustomFabricProduct WHERE custom_fabric_product_id = $1';
-
-    connection.query(sql, [customFabricId], (err, result) => {
-        if (err) {
-            console.error('Error executing SQL query:', err);
-            res.status(500).send('Internal Server Error');
-        } else {
-            if (result.length > 0) {
-                res.json(result[0]);
-            } else {
-                res.status(404).send('Product not found');
-            }
-        }
-    });
-});
-
-
-//View Seller by product
-app.get('/seller/:id/products/:offset', async (req, res) => {
-    try {
-        const userProvidedOffset = parseInt(req.params.offset, 10) || 0;
-        const seller_email = req.params.id;
-        const sql = 'SELECT * FROM Product WHERE seller_email = $1 ORDER BY date LIMIT 10 OFFSET $2';
-        const result = await connection.query(sql, [seller_email, userProvidedOffset]);
-        res.json(result.rows);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({error: 'Internal Server Error'});
-    }
-});
 
 
 app.post('/record-payment', async (req, res) => {
-    try{
     const {payment_id, payment_provider, seller_email, customer_email, transaction_amount, transaction_datetime, payer_number} = req.body;
-    const sql = 'INSERT INTO Payment(payment_id, payment_provider, seller_email, customer_email, transaction_amount, transaction_datetime, payer_number) VALUES(?,?,?,?,?,?,?)';
-    const result = await connection.query(sql, [payment_id, payment_provider, seller_email, customer_email, transaction_amount, transaction_datetime, payer_number]);
-      res.json(result.rows);
-  } catch (error) {
-      console.error(error);
-      res.status(500).json({error: 'Internal Server Error'});
-  }
-});
-
-
-//Merchant get's to view all their orders based on status
-app.get('/merchant-order/:status/:offset', async (req, res) => {
-    try {
-        const userProvidedOffset = parseInt(req.params.offset, 10) || 0;
-        const order_status = req.params.status;
-        const seller_email = req.body;
-        const sql = 'SELECT Orders.*, MerchantProductOrder.* FROM Orders JOIN MerchantProductOrder ON Orders.payment_id = MerchantProductOrder.payment_id WHERE Orders.seller_email = $1 ORDER BY Orders.date_created LIMIT 10 OFFSET $2';
-        const result = await connection.query(sql, [seller_email, order_status, userProvidedOffset]);
-        res.json(result.rows);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({error: 'Internal Server Error'});
-    }
-});
-
-
-//Woven gets to see their stole order based on status
-app.get('/stole-order/:status/:offset', async (req, res) => {
-    try {
-        const order_status = req.params.status;
-        const userProvidedOffset = parseInt(req.params.offset, 10) || 0;
-        const admin_email = req.body;
-        const sql = 'SELECT Orders.*, StoleProductOrder.* FROM Orders JOIN StoleProductOrder ON Orders.payment_id = StoleProductOrder.payment_id WHERE Orders.admin_email = $1 ORDER BY Orders.date_created LIMIT 10 OFFSET $2';
-        const result = await connection.query(sql, [admin_email, userProvidedOffset]);
-        res.json(result.rows);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({error: 'Internal Server Error'});
-    }
-});
-
-
-//Woven gets to see their stole order based on status
-app.get('/kente-order/:status/:offset', async (req, res) => {
-    try {
-        const order_status = req.params.status;
-        const userProvidedOffset = parseInt(req.params.offset, 10) || 0;
-        const admin_email = req.body;
-        const sql = 'SELECT Orders.*, CustomFabricOrderDetails.* FROM Orders JOIN CustomFabricOrderDetails ON Orders.payment_id = CustomFabricOrderDetails.payment_id WHERE Orders.admin_email = $1 ORDER BY Orders.date_created LIMIT 10 OFFSET $2';
-        const result = await connection.query(sql, [admin_email, userProvidedOffset]);
-        res.json(result.rows);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({error: 'Internal Server Error'});
-    }
-});
+    connection.query('INSERT INTO Payment(payment_id, payment_provider, seller_email, customer_email, transaction_amount, transaction_datetime, payer_number) VALUES ($1,$2,$3,$4,$5,$6,$7)',[payment_id, payment_provider, seller_email, customer_email, transaction_amount, transaction_datetime, payer_number], (error, results) => {
+        if (error) {
+          throw error
+        }
+        res.status(201).send(true);
+      });
+    });
 
 
 //View All Sellers Products
-app.get('/sellers/products/:offset', async (req, res) => {
-    try {
-        const userProvidedOffset = parseInt(req.params.offset, 10) || 0;
-        const seller_email = req.body;
-        const sql = 'SELECT * FROM Product WHERE seller_eamil = $1 ORDER BY date LIMIT 10 OFFSET $2';
-        const result = await connection.query(sql, [seller_email, userProvidedOffset]);
-        res.json(result.rows);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({error: 'Internal Server Error'});
-    }
+app.get('/sellers/products/:email', async (req, res) => {
+    const seller_email = req.params['email'] 
+    connection.query('SELECT * FROM Product WHERE seller_email = $1', [seller_email], (error, results) => {
+        if (error) {
+            throw error;
+        }
+        res.status(200).json(results); 
+    });
 });
+
 
 //Adding a new merchant product
 app.post('/sellers/products/', async (req, res) => {
-        
-        const {seller_email, product_name, yards, description, price, image_link} = req.body;
-        const sql = 'INSERT INTO Product(seller_email, product_name, yards, description, price, image_link) VALUES(?,?,?,?,?,?)';
-        const result = await connection.query(sql, [seller_email, product_name, yards, description, price, image_link]);
-        res.json(result.rows);
-    
-});
-
-
-
-
-
-
-
+    connection.query('INSERT INTO Prouct(seller_email, product_name, yards, description, price, image_link) VALUES ($1,$2,$3,$4,$5,$6)', [seller_email, product_name, yards, description, price, image_link], (error, results) => {
+        if (error) {
+          throw error
+        }
+        res.status(201).send('Merchant product succesfuly added');
+      })
+    });
 
 
 //Adding a new custom fabric
 app.post('/admin/customfabric', async (req, res) => {
-  try {
-      const {admin_email, description, price, image_link} = req.body;
-      const sql = 'INSERT INTO CustomFabricProduct(admin_email, description, price, image_link) VALUES(?,?,?,?)';
-      const result = await connection.query(sql, [admin_email, description, price, image_link]);
-      res.json(result.rows);
-  } catch (error) {
-      console.error(error);
-      res.status(500).json({error: 'Internal Server Error'});
-  }
-});
+    connection.query('INSERT INTO CustomFabricProduct(admin_email, description, price, image_link) VALUES($1,$2,$3,$4)',[admin_email, description, price, image_link], (error, results) => {
+        if (error) {
+          throw error
+        }
+        res.status(201).send('Custom Fabric successfully added');
+      })
+    });
+  
 
 
 app.listen(port, () => {
